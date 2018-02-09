a94941326bf3c64b4f6213def7a8f45656facfa9
Ben <benjohn13@protonmail.com>
2018-02-09 11:24:03 -0600

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+with open('README.rst', 'r') as f:
+    readme = f.read()
+
+setup(
+    name='pgbackup',
+    version='0.1.0',
+    description='Database backups to AWS S3 or locally.',
+    long_description=readme,
+    author='Ben J',
+    author_email='example@email.com'.
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    install_requires=['boto3'],
+    entry_points={
+        'console_scripts': [
+            'pgbackup=pgbackup.cli:main',
+        ]
+    }
+)
